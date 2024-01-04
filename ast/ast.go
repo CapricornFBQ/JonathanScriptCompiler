f026aecc960094000c752d5bda072d6d2fdc67da
Jonathan <979641996@qq.com>
2024-01-05 00:08:32 +0800

    complete parser

--- ast/ast.go
+++ ast/ast.go
@@ -21,8 +21,8 @@ type Program struct {
 }
 
 func (p *Program) TokenLiteral() string {
-	if len(p.Statement) > 0 {
-		return p.Statement[0].TokenLiteral()
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
@@ -37,6 +37,7 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode() {
 
 }
+
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
@@ -49,6 +50,7 @@ type Identifier struct {
 func (i *Identifier) expressionNode() {
 
 }
+
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
