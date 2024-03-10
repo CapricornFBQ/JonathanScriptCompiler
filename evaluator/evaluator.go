a3bffd2049821a8685617155c6f266d48213beb3
Jonathan <979641996@qq.com>
2024-03-10 14:39:35 +0800

    add boolean unit test and evaluator;

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,17 @@ import (
 	"jonathan/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	// Statements
@@ -14,6 +25,8 @@ func Eval(node ast.Node) object.Object {
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 	//  another implement
 	//case *ast.ExpressionStatement:
