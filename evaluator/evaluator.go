7af16b1eee8e637c6eb4928e15e4244c50d3851a
Jonathan <979641996@qq.com>
2024-03-14 22:40:57 +0800

    add if statement evoluation;

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -35,6 +35,11 @@ func Eval(node ast.Node) object.Object {
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
+
 	}
 	//  another implement
 	//case *ast.ExpressionStatement:
@@ -123,5 +128,27 @@ func evalIntegerInfixExpression(operator string,
 		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
+	}
+}
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
