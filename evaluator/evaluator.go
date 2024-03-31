788099c1f983df90380be3e95ecc0ef38832b90f
Jonathan <979641996@qq.com>
2024-03-31 20:01:53 +0800

    add array index evaluator;

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -75,6 +75,16 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return elements[0]
 		}
 		return &object.Array{Elements: elements}
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	case *ast.CallExpression:
 		function := Eval(node.Function, env)
 		if isError(function) {
@@ -281,6 +291,25 @@ func evalExpressions(
 	return result
 }
 
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ArrayObj && index.Type() == object.IntegerObj:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	maxIndex := int64(len(arrayObject.Elements) - 1)
+	if idx < 0 || idx > maxIndex {
+		return NULL
+	}
+	return arrayObject.Elements[idx]
+}
+
 func applyFunction(fn object.Object, args []object.Object) object.Object {
 	switch fn := fn.(type) {
 	case *object.Function:
@@ -293,6 +322,7 @@ func applyFunction(fn object.Object, args []object.Object) object.Object {
 		return newError("not a function: %s", fn.Type())
 	}
 }
+
 func extendFunctionEnv(fn *object.Function, args []object.Object,
 ) *object.Environment {
 	env := object.NewEnclosedEnvironment(fn.Env) // the fn.Evn it the outer. It's a linked list.
