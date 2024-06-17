a2ed185da5c4ddd1e81b56f4f6b53cd34a1e54a9
Jonathan <979641996@qq.com>
2024-06-17 23:23:17 +0800

    complete the add op in compiler and vm;

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,6 +1,7 @@
 package compiler
 
 import (
+	"fmt"
 	"jonathan/ast"
 	"jonathan/code"
 	"jonathan/object"
@@ -39,6 +40,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 		err = c.Compile(node.Right)
 		if err != nil {
 			return err
+		}
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("unknown operator %s", node.Operator)
 		}
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
--- vm/vm.go
+++ vm/vm.go
@@ -43,6 +43,18 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpAdd:
+			right := vm.pop()
+			left := vm.pop()
+			leftValue := left.(*object.Integer).Value
+			rightValue := right.(*object.Integer).Value
+			result := leftValue + rightValue
+			err := vm.push(&object.Integer{Value: result})
+			if err != nil {
+				return err
+			}
+		default:
+			return nil
 		}
 	}
 	return nil
@@ -55,4 +67,10 @@ func (vm *VM) push(o object.Object) error {
 	vm.stack[vm.sp] = o
 	vm.sp++
 	return nil
+}
+
+func (vm *VM) pop() object.Object {
+	o := vm.stack[vm.sp-1]
+	vm.sp--
+	return o
 }
