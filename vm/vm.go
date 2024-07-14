d522194dd29f243711f35af98b32a96ba72d4d59
Jonathan <979641996@qq.com>
2024-07-14 22:49:10 +0800

    vm deal with `if` struction;

--- vm/vm.go
+++ vm/vm.go
@@ -66,6 +66,16 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+		case code.OpJumpNotTruthy:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+			condition := vm.pop()
+			if !isTruthy(condition) {
+				ip = pos - 1
+			}
+		case code.OpJump:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip = pos - 1 // change the next instructions
 		case code.OpBang:
 			err := vm.executeBangOperator()
 			if err != nil {
@@ -195,4 +205,13 @@ func (vm *VM) pop() object.Object {
 
 func (vm *VM) LastPoppedStackElem() object.Object {
 	return vm.stack[vm.sp]
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj := obj.(type) {
+	case *object.Boolean:
+		return obj.Value
+	default:
+		return true
+	}
 }
