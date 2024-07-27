da30afa5889763888089648369f08ee38628f460
Jonathan <979641996@qq.com>
2024-07-27 23:28:39 +0800

    The compiler adds logic for processing arrays

--- code/code.go
+++ code/code.go
@@ -32,6 +32,8 @@ const (
 
 	OpGetGlobal
 	OpSetGlobal
+
+	OpArray
 )
 
 type Definition struct {
@@ -59,6 +61,7 @@ var definitions = map[Opcode]*Definition{
 	OpNull:          {"OpNull", []int{}},
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
+	OpArray:         {"OpArray", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
