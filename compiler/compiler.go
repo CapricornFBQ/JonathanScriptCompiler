9dae2cece654c1722d4b07a826a50bfb1df6b76c
Jonathan <979641996@qq.com>
2024-08-25 23:21:55 +0800

    add logic for call instruction compiling

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -248,6 +248,13 @@ func (c *Compiler) Compile(node ast.Node) error {
 			return err
 		}
 		c.emit(code.OpReturnValue)
+	case *ast.CallExpression:
+		err := c.Compile(node.Function)
+		if err != nil {
+			return err
+		}
+		c.emit(code.OpCall)
+
 	}
 	return nil
 }
