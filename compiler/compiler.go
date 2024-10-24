5233c963a7c8568c86616f1a2e5578ec50a19a07
Jonathan <979641996@qq.com>
2024-10-24 19:00:40 +0800

    add builtin instruction emit

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -37,9 +37,13 @@ func NewCompiler() *Compiler {
 		lastInstruction:     EmittedInstruction{},
 		previousInstruction: EmittedInstruction{},
 	}
+	symbolTable := NewSymbolTable()
+	for i, v := range object.Builtins {
+		symbolTable.DefineBuiltin(i, v.Name)
+	}
 	return &Compiler{
 		constants:   []object.Object{},
-		symbolTable: NewSymbolTable(),
+		symbolTable: symbolTable,
 		scopes:      []CompilationScope{mainScope},
 		scopeIndex:  0,
 	}
@@ -73,11 +77,7 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if !ok {
 			return fmt.Errorf("undefined variable %s", node.Value)
 		}
-		if symbol.Scope == GlobalScope {
-			c.emit(code.OpGetGlobal, symbol.Index)
-		} else {
-			c.emit(code.OpGetLocal, symbol.Index)
-		}
+		c.loadSymbol(symbol)
 	case *ast.LetStatement:
 		// Note: compile expression first. if the value is a integer. the previous instruction is a integer.
 		// the integer will be pushed into the stack in the vm. so, when the vm get the OpSetGlobal ,
@@ -391,6 +391,18 @@ func (c *Compiler) leaveScope() code.Instructions {
 	c.scopeIndex--
 	c.symbolTable = c.symbolTable.Outer // come back to the outer symbol table of scope
 	return instructions
+}
+
+func (c *Compiler) loadSymbol(s Symbol) {
+	switch s.Scope {
+	case GlobalScope:
+		c.emit(code.OpGetGlobal, s.Index)
+	case LocalScope:
+		c.emit(code.OpGetLocal, s.Index)
+	case BuiltinScope:
+		c.emit(code.OpGetBuiltin, s.Index)
+	default:
+	}
 }
 
 func (c *Compiler) PrintStatements() {
